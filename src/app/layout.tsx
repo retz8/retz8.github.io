583b1993c8b00dc83a262b83abc21ab1c356a7a0
retz8 <ekkicb71@gmail.com>
2024-08-26 19:06:15 -0400

    dmddo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import ProfileCard from "@/components/ProfileCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* outer div: define background (full screen) */}
+          <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+            {/* inner div: header - main alignment */}
+            <div className="lg:flex lg:justify-between lg:gap-4">
+              <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+                {/* Profile Card --- social media icons  */}
+                <ProfileCard />
+              </header>
+
+              {/* main contents (from page.tsx) */}
+              {children}
+            </div>
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ProfileCard() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">Jioh In</h1>
+      <h2 className="mt-3 text-lg font-medium tracking-tight sm:text-xl">
+        Full Stack Developer | CS Student at University of Michigan
+      </h2>
+      <p className="mt-4 max-w-xs leading-norma text-foreground/70">
+        I build ... something
+      </p>
+    </div>
+  );
+}
