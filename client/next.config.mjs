a2df58803a0fdaecb08bff86eed0998aedc6f133
retz8 <ekkicb71@gmail.com>
2025-05-07 16:38:24 -0400

    base setup for react + vite

--- client/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // enables static exports
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
