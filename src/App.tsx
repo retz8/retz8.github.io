c14e4e3088ea15d80b42237fb6fc504dbfa78076
retz8 <ekkicb71@gmail.com>
2025-05-09 19:07:44 -0400

    rollback

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
 
 import "./App.css";
 
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 // Pages
@@ -15,13 +15,13 @@ import CookPage from "@/domains/cook/CookPage";
 export default function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<PortfolioPage />} />
           <Route path="/blog" element={<BlogPage />} />
           <Route path="/cook" element={<CookPage />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
